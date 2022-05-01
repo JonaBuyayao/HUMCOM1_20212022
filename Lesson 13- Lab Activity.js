8441cc53cda4a4071918edf730df6a3a1ea81618
JonaBuyayao_HUMCOM1_20212022 <98310528+JonaBuyayao@users.noreply.github.com>
2022-05-01 14:00:04 +0800

    Add files via upload

---
+++ Lesson 13- Lab Activity.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Lesson 13- Lab Activity" content="Lesson 13- Lab Activity">
+        <meta name="keywords" content="HTML, JavaScript">
+        <meta name="Buyayao, Jona, M." content="Lesson 13- Lab Activity">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Javascript</title>
+    </head>
+    <body>
+        <script>
+            for(i=1;i<6;i++){
+                document.write("This is iteration" + i + "<br>");
+            }
+        </script>   
+    </body>
+</html>
