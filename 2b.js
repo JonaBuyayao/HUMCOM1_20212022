a2389de10aceeb015313113324d9bb0e8636c588
JonaBuyayao_HUMCOM1_20212022 <98310528+JonaBuyayao@users.noreply.github.com>
2022-05-04 19:04:14 +0800

    Delete 2b.js

--- 2b.js
+++
@@ -1,5 +0,0 @@
-document.write("Jona Malafu Buyayao <br />");
-document.write("BS Information Technology <br />");
-document.write("1st Year <br />"); 
-document.write("HUMCOM1-IAA2 <br />"); 
-document.write("Introduction to Human Computer Interaction <br />"); 
