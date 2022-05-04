40109fb127de73a42e633d99818130b9268fe4b1
JonaBuyayao_HUMCOM1_20212022 <98310528+JonaBuyayao@users.noreply.github.com>
2022-05-04 19:04:28 +0800

    Delete 2c.js

--- 2c.js
+++
@@ -1,5 +0,0 @@
-console.log("Jona Malafu Buyayao");
-console.log("BS Information Technology");
-console.log(2-1);
-console.log("HUMCOM1-IAA2");
-console.log("Introduction to Human Computer Interaction");
