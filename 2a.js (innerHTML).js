02d6cdd6b13afa47c2e12396d054d61c199ade6d
JonaBuyayao_HUMCOM1_20212022 <98310528+JonaBuyayao@users.noreply.github.com>
2022-05-04 19:08:08 +0800

    Add files via upload

---
+++ 2a.js (innerHTML).js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML="Jona Malafu Buyayao";
+document.getElementById("course").innerHTML="BS Information Technology";
+document.getElementById("year").innerHTML="1st Year";
+document.getElementById("subject code").innerHTML="HUMCOM1-IAA2";
+document.getElementById("subject description").innerHTML="Introduction to Human Computer Interaction";
